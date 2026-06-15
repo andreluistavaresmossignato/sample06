8fe46c388dff27c7d1284aed37195ee14dd9b602
Aluno <aluno@fatecmm.local>
2026-06-15 15:13:20 -0300

    CSS Estilo Atualizado

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,25 @@
 'use client'
 
-import { useState } from "react"
-import "./page.css"
+import { useState } from "react";
+import "./page.css";
 
 export default function Home() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='painel'>
-      <h1 className='titulo'>Contador: {count}</h1>
-      <button className='botao' onClick={() => setCount(count + 1)}>
-        Incrementar
-      </button>
-    </div>
+    <main className="painel">
+      <div className="contador-card">
+        <span className="label">CONTADOR</span>
+
+        <h1 className="titulo">{count}</h1>
+
+        <button
+          className="botao"
+          onClick={() => setCount(count + 1)}
+        >
+          Incrementar
+        </button>
+      </div>
+    </main>
   );
 }
